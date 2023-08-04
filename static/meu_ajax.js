bc0ee699f01bb13f3b987270d1b34e563304e4e3
rodrig20 <rodrigo.f.lopes2004@gmail.com>
2023-08-04 17:11:55 +0100

    Adicionar os pedidos self-service Atravez de QrCode

--- static/meu_ajax.js
+++ static/meu_ajax.js
@@ -20,14 +20,14 @@ function addRow(c,opcoes) {
         var my_opcoes = opcoes[1]
     }
     cell1.innerHTML = "<input type='number' style='border: 3px solid " + c + ";' id='quantity' class='quantity' min='1'>";
-    cell2.innerHTML = "<select style='border: 3px solid " + c + ";' id='pedido' class='pedido'></select>";
+    cell2.innerHTML = "<select style='border: 3px solid " + c + ";' id='pedido' class='pedido'><option value=''></option></select>";
     cell3.innerHTML = "<input type='button' id='cancelar' onclick='removerPedido(" + r + ")' value=' x '>"
     cell4.innerHTML = "<input type='hidden' class='tipo' value='" + p + "'>"
     cell1.className = 'quantiNum';
     // Adicionar as opções válidas ao select
     var select = cell2.getElementsByTagName("select")[0];
     for (let i = 0; i < my_opcoes.length; i++) {
-        var option = document.createElement("option");
+        option = document.createElement("option");
         option.text = my_opcoes[i];
         option.value = my_opcoes[i];
         select.add(option);
@@ -35,6 +35,38 @@ function addRow(c,opcoes) {
 
     r++;
 };
+
+function addPreSetRow(opcoes,q,p,t){
+    var table = document.getElementById("tabela");
+    var row = table.insertRow(-1);
+    row.setAttribute("id",r);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    if (t == 'Comida') {
+        c = '#1ea831'
+        var my_opcoes = opcoes[0]
+    } else {
+        c = '#292b94'
+        var my_opcoes = opcoes[1]
+    }
+    cell1.innerHTML = "<input type='number' value="+q+" style='border: 3px solid " + c + ";' id='quantity' class='quantity' min='1'>";
+    cell2.innerHTML = "<select style='border: 3px solid " + c + ";' id='pedido' class='pedido'></select>";
+    cell3.innerHTML = "<input type='button' id='cancelar' onclick='removerPedido(" + r + ")' value=' x '>"
+    cell4.innerHTML = "<input type='hidden' class='tipo' value='" + t + "'>"
+    cell1.className = 'quantiNum';
+    var select = cell2.getElementsByTagName("select")[0];
+    for (let i = 0; i < my_opcoes.length; i++) {
+        var option = document.createElement("option");
+        option.text = my_opcoes[i];
+        option.value = my_opcoes[i];
+        select.add(option);
+    }
+    select.value = p
+    
+    r++;
+}
 
 function removerPedido(n){
     var row = document.getElementById(n);
@@ -112,18 +144,28 @@ function decodeQR() {
     var code = jsQR(imageData.data, imageData.width, imageData.height);
     if (code) {
       // Se um código QR foi decodificado, exibir o resultado
-      console.log(code.data)
-
+      sendQrdata(code.data);
     } else {
       // Se nenhum código QR foi encontrado, tentar novamente no próximo frame
       requestAnimationFrame(decodeQR);
     }
-  }
+}
 
 function toQRScanner(){
     nome = $('input[type="hidden"]').val();
     $(location).prop('href', $SCRIPT_ROOT + '/QRScanner/' + nome);
     //window.location.href = "qrcode.html"
+}
+
+function sendQrdata(data){
+    json_send = {pedB64:data}
+    $.post($SCRIPT_ROOT, json_send, volatServico,'json');
+}
+
+function volatServico(data){
+    if (data.suc == "T"){
+        $(location).prop('href', data['redirect']);
+    }
 }
 
 // Início do jQuery:
