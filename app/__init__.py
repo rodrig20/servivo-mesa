3a4564f82db493d717f84ff281b2001f2a7eeb7f
rodrig20 <rodrigo.f.lopes2004@gmail.com>
2023-08-04 22:28:12 +0100

    Simplificar

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,25 @@
-import eventlet
-eventlet.monkey_patch()
-
-from flask import Flask
+from engineio.async_drivers import gevent
+from app.password import get_secret_key, get_password
 from flask_socketio import SocketIO
+from typing import List, Callable
 from flask_caching import Cache
 from datetime import timedelta
-from .loophole import *
 from threading import Thread
+from flask import Flask
+from .loophole import *
 import os.path
 import json
 
-def atualizarRotas(dicionario):
+
+def atualizarRotas(dicionario: dict) -> None:
     app.config["Comida"] = dicionario.get("cozinha", False)
     app.config["Bebida"] = dicionario.get("bar", False)
     app.config["QrCode"] = dicionario.get("QrCode", False)
 
-app = Flask(__name__)
 
-app.config['ASYNC_MODE'] = 'threading'
-app.config['SECRET_KEY'] = 'Rio_'
+app = Flask(__name__)
+app.config['async_mode'] = 'gevent'
+app.config['SECRET_KEY'] = get_secret_key()
 app.config['CACHE_TYPE'] = 'simple'
 app.config['CACHE_KEY_PREFIX'] = 'file_'  # Prefixo para chaves de cache
 app.config['SESSION_REFRESH_EACH_REQUEST'] = True
@@ -27,18 +28,18 @@ app.config['SESSION_COOKIE_HTTPONLY'] = True
 
 cache = Cache(app)
 
-socketio= SocketIO(app, async_mode='eventlet', cors_allowed_origins="*")
-
+socketio = SocketIO(app, async_mode="gevent", cors_allowed_origins="*")
 app.config["USERS"] = []
 app.config["MENU_NAME"] = [[],[]]
 app.config["MENU_PRICE"] = [[],[]]
-app.config["URLS"] = {}
+atualizarRotas({})
 
-password = "Entrar"
+app.config["PASSWORD"] = ""
+app.config["MAX_PASSWORD_TRIES_PER_MINUTE"] = 10
+app.config["PASSWORD_TRIES"] = {}
 
-def run_app(config,progress_callback):
+def run_app(config_server) -> Callable:
     config_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\config\\"
-    progress_callback(5)
     try:
         with open(config_path+"users.txt", 'r+',encoding="utf-8") as u:
             users = u.readlines()
@@ -46,7 +47,7 @@ def run_app(config,progress_callback):
             app.config["USERS"].append(user[:-1])  
     except FileNotFoundError:
         with open(config_path+"users.txt", 'w',encoding="utf-8"): pass
-    progress_callback(10)
+    
     app.config["USERS"].append("cozinha")
     app.config["USERS"].append("bar")
     try:
@@ -55,43 +56,48 @@ def run_app(config,progress_callback):
         for name, price in menu["Comida"].items():
             app.config["MENU_NAME"][0].append(name)
             app.config["MENU_PRICE"][0].append(price)
-        progress_callback(14)
         for name, price in menu["Bebida"].items():
             app.config["MENU_NAME"][1].append(name)
             app.config["MENU_PRICE"][1].append(price)
             
     except FileNotFoundError:
         with open(config_path+"menu.json", 'w',encoding="utf-8"): pass
-    progress_callback(16)
-    try:
-        with open(config_path+"network_access.json", 'r+',encoding="utf-8") as u:
-            acessos = json.load(u)
-        app.config["URLS"]["cozinha"] = acessos["enable_Cozinha"]
-        app.config["URLS"]["bar"] = acessos["enable_Bar"]
-        app.config["URLS"]["QrCode"] = acessos["enable_QrCode"]
-        port = acessos["port"]
-        progress_callback(15)
-        if acessos["enable_local"]:
-            host = "0.0.0.0"
-            config.links.append("0.0.0.0")
-        else:
-            config.links.append("127.0.0.1")
-            host = "localhost"
-        progress_callback(10)
-        if acessos["enable_loophole"]:
-            config.links.append(acessos["domain"])
-            Thread(target=start_loophole, args=(config.links,port,progress_callback),daemon=True).start()
-        else:
-            progress_callback(15)
-    except FileNotFoundError:
-        with open(config_path+"network_access.json", 'w',encoding="utf-8"): pass
-        app.config["URLS"]["cozinha"] = 1
-        app.config["URLS"]["bar"] = 0
-        app.config["URLS"]["QrCode"] = 0
-        host = "localhost"
-        port = 8080
-    progress_callback(15)
-    atualizarRotas(app.config["URLS"])
-    socketio.run(app,host=host,port=port)
+    
+    if "loophole" in config_server.type:
+        domain = config_server.urls[config_server.type.index("loophole")]
+        
+    else:
+        domain = "pass/Word"
+        
+        
+    host = config_server.host
+    port = config_server.port
+    loop = 0
+    for i in range(len(config_server.urls)):
+        if config_server.type[i] == "loophole":
+            Thread(target=start_loophole,args=(config_server,port,i),daemon=True).start()
+            loop = 1
+    if not loop:
+        config_server.final = 1
+    atualizarRotas(config_server.access)
+    
+    app.config["PASSWORD"] = get_password(domain)
+    config_server.password = app.config["PASSWORD"]
+    createUserNamespace("/atualizarProntos",app.config["USERS"])
+    createListNamespace("/atualizarEspera")
+    return lambda: socketio.run(app,host=host,port=port)
 
 from app import routes
+
+def createUserNamespace(basic_namespace: str, user_list: List[str]) -> None:
+    for user in user_list:
+        socketio.on_namespace(routes.AtualizarPagina(basic_namespace+"/"+user))
+
+def createListNamespace(basic_namespace: str) -> None:
+    if app.config["Comida"]:
+        socketio.on_namespace(routes.AtualizarPagina(basic_namespace+"/Comida"))
+    if app.config["Bebida"]:
+        socketio.on_namespace(routes.AtualizarPagina(basic_namespace+"/Bebida"))
+        
+app.config["PEDIDOS_ESPERA"] = routes.ListaTodosPedidos_Espera()
+app.config["PEDIDOS_PRONTOS"] = routes.ListaTodosPedidos_Prontos()
