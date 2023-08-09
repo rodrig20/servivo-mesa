5bdd481360a4d85a5587cfcc86bb0f6245ebde7c
rodrig20 <rodrigo.f.lopes2004@gmail.com>
2023-08-09 17:49:14 +0100

    Permitir um inicio rápido com as configs atuais (sem GUI)

--- main.py
+++ main.py
@@ -2,8 +2,43 @@ import ctypes
 ctypes.windll.user32.SetProcessDPIAware()
 
 from interface import start_window
+import argparse
 from app import *
 
 
 if __name__ == "__main__":
-    start_window(run_app)
+    # Criar um objeto ArgumentParser
+    parser = argparse.ArgumentParser(description='App para serviço de mesa')
+
+    # Adicionar a flag --fast-start
+    parser.add_argument('--fast-start', action='store_true', help='Iniciar app sem necessidade de GUI')
+
+    # Fazer o parsing dos argumentos da linha de comando
+    args = parser.parse_args()
+    if args.fast_start:
+        from config.config import ConfigServer
+        from threading import Thread
+        import signal
+        import json
+        import time
+        import os
+        
+        config = ConfigServer()
+        with open(config.config_path+"network_access.json") as na:
+            info = json.load(na)
+        config.prepare_config(info)
+        fun = run_app(config)
+        print(config.password)
+        
+        thread = Thread(target=fun)
+        thread.start()
+        input("-> Enter para Parar \n")
+        if "loophole" in config.type:
+            config.run_loophole = 0
+            while config.run_loophole != 2:
+                time.sleep(0.1)
+                    
+        os.kill(os.getpid(), signal.SIGINT)   
+        
+    else:
+        start_window(run_app)
