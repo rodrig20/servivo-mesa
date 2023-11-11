719b7a414cddbab50686eb8f8d123e6811a9634d
rodrig20 <rodrigo.f.lopes2004@gmail.com>
2023-11-11 17:07:43 +0000

    Melhorar o comando usado para criar o executavel

--- to_exe.py
+++ to_exe.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 dir_path = str(Path.cwd()).replace("\\", "/")
 
-comando = 'pyinstaller --noconfirm --onedir --windowed --exclude-module PyQt5 --exclude-module tkinter --exclude-module _tkinter --add-data "var(dir)/app;app/" --add-data "var(dir)/config;config/" --add-data "var(dir)/interface;interface/" --icon="var(dir)/interface/Ui/images/icon.ico" --distpath "var(dir)/executavel/dist" --workpath "var(dir)/executavel/build" --specpath "var(dir)/executavel" "var(dir)/main.py"'
-comando = comando.replace("var(dir)", dir_path)
+comando = f'pyinstaller --noconfirm --onedir --windowed --exclude-module PyQt5 --exclude-module tkinter --exclude-module _tkinter --add-data "{dir_path}/app;app/" --add-data "{dir_path}/config;config/" --add-data "{dir_path}/interface;interface/" --icon="{dir_path}/interface/Ui/images/icon.ico" --distpath "{dir_path}/executavel/dist" --workpath "{dir_path}/executavel/build" --specpath "{dir_path}/executavel" "{dir_path}/main.py"'
+
 
 process = Popen(comando.strip())
 process.wait()
